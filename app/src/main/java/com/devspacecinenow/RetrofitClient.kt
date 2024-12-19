32ce21824fdb61f30ae108b4349a7d8d37989029
eduardoagabes <eduardoagabes@hotmail.com>
2024-12-19 12:56:33 +0100

    creating async image and creating movie session

--- app/src/main/java/com/devspacecinenow/RetrofitClient.kt
+++ app/src/main/java/com/devspacecinenow/RetrofitClient.kt
@@ -5,14 +5,14 @@ import okhttp3.Request
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-private const val BASE_URL: String = "https://api.themoviedb.org/3/movie"
+private const val BASE_URL: String = "https://api.themoviedb.org/3/movie/   "
 
 object RetrofitClient {
 
     private val httpClient: OkHttpClient
         get() {
             val clienteBuilder = OkHttpClient.Builder()
-            val token = ""
+            val token = BuildConfig.API_KEY
 
             clienteBuilder.addInterceptor { chain ->
                 val original: Request = chain.request()
